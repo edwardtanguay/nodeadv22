fe581ab6a8b9612be855944cf8ab2f8c9e7a869a
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-19 20:50:58 +0100

    randompromise

--- randomPromise.js
+++ randomPromise.js
@@ -8,15 +8,9 @@ const randomNumber = (start, end) => {
 
 const main = async function () {
 	for (let x = 0; x < 4; x++) {
-		console.log(await randomNumber(0, 9));
-	}
-}
-
-const main2 = async function () {
-	for (let x = 0; x < 4; x++) {
+		// console.log(await randomNumber(0, 9)); // same as below
 		await randomNumber(0, 9).then(rn => console.log(rn));
 	}
 }
 
 main();
-main2();
