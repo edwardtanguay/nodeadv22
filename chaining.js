1b7ad0640cd80251370e8202f290bfaec17289b9
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-23 00:57:02 +0100

    made chaining more complex

--- chaining.js
+++ chaining.js
@@ -1,13 +1,16 @@
-const loginUser = () => {
+const loginUser = (id) => {
 	return new Promise((resolve, reject) => {
 		const randomNumber = Math.floor(Math.random() * 2) + 1;
 		if (randomNumber === 2) {
 			reject({
-				message: 'bad login'
+				message: `bad login for id ${id}`
 			});
 		} else {
 			setTimeout(() => {
-				resolve();
+				resolve({
+					state: 'successful',
+					id
+				});
 			}, 1000);
 		}
 	});
@@ -25,13 +28,11 @@ const getUserData = (id) => {
 	});
 };
 
-loginUser().then(() => {
-	console.log('user is logged in');
-}).then(() => {
-	getUserData(42).then((data) => {
+loginUser(43).then((response) => {
+	console.log(`user ${response.id} is logged in`);
+	getUserData(response.id).then((data) => {
 		console.log(`got user data for "${data.lastName}, ${data.firstName}" (#${data.id})`);
 	});
 }).catch((e) => {
 	console.log('error: ' + e.message);
 });
-
